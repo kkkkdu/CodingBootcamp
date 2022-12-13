2ca21722c5452ee72f655bc278b5feffa08aecb9
Carlos <minesottile@gmail.com>
2022-12-13 16:22:32 -0300

    Realizando meu primeiro commit

---
+++ aula.js
@@ -0,0 +1,12 @@
+const PreçoCombustivel = 5.80;
+let Percurso = 1258;
+let GastoVeiculo = 4;
+
+let GastoDistancia = Percurso/GastoVeiculo;
+let PreçoPorLitro = GastoDistancia*PreçoCombustivel;
+
+
+
+console.log("Foi gasto: " + GastoDistancia  + " Litros neste percurso");
+console.log("O combustivel está R$ " + PreçoCombustivel + " Por km");
+console.log("No total foram gastos R$" + PreçoPorLitro.toFixed(2) + " para concluir toda a viagem");
